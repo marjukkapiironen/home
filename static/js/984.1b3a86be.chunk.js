8f4eea4c7007d4871d240ee8117bf980f2c19e7c
Marjukka <emapi007@edu.xamk.fi>
2024-01-29 23:00:48 +0200

    testing

---
+++ static/js/984.1b3a86be.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[984],{5984:(e,o,r)=>{r.r(o),r.d(o,{default:()=>p});var s=r(2933),t=r(6267),f=r(5508),l=r(8934);const p={...{renderer:r(1277).b,...f.s,...l.E},...s.o,...t.b}}}]);
+//# sourceMappingURL=984.1b3a86be.chunk.js.map
